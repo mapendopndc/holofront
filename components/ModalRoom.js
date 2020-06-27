be096243bc9b8c1a5d14f61ea6808594a5fcf264
Mapendo Ngilinga de Carvalho <mapendopndc@gmail.com>
2020-06-27 11:00:50 -0400

    Add room list with dummy data

---
+++ components/ModalRoom.js
@@ -0,0 +1,79 @@
+import { connect } from 'react-redux'
+import { authenticate } from '../store/actions/authActions'
+import { register } from '../store/actions/authActions'
+import { Modal, Form, Button } from 'react-bootstrap'
+
+class ModalRoom extends React.Component {
+
+    state = {
+        email: '',
+        show: false
+    }
+
+    componentDidUpdate() {
+        if (this.props.show) {
+            this.setState({
+                ...this.state,
+                show: true
+            })
+        }
+    }
+
+    handleClose = () => {
+        this.setState({
+            show: false
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        // this.props.add_friend(roomInfo)
+
+        const authForm = document.getElementById("authForm")
+        authForm.reset()
+        this.setState({
+            ...this.state,
+            email: ''
+        })
+
+        this.handleClose(); // Bug: Must only close if successfully authenticated
+    }
+
+    render() {
+        return (
+            <Modal show={this.state.show} onHide={this.handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add Friend to Room</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={this.handleSubmit} id="authForm">
+                    <Form.Group>
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter your friend's email address" name="email" onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit" className="float-right">
+                        Invite
+                    </Button>
+                </Form>
+            </Modal.Body>
+            </Modal>
+        )
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        authenticate: (userInfo) => { dispatch(authenticate(userInfo))},
+        register: (userInfo) => { dispatch(register(userInfo))}
+    }
+}
+
+export default connect(null, mapDispatchToProps)(ModalRoom);
--- components/RoomList.js
+++ components/RoomList.js
@@ -1,11 +1,51 @@
+import { Button, Card } from 'react-bootstrap'
 import { connect } from 'react-redux'
+import ModalRoom from './ModalRoom'
 
 class RoomList extends React.Component {
+
+    state = {
+        show: false,
+        dummy: [
+            {id: 1, name: 'black swan'},
+            {id: 2, name:' bumblebee'},
+            {id: 3, name: 'pineapple'}
+        ]
+    }
+
+    handleShow = (roomInfo) => {
+        console.log("You clicked on " + roomInfo.name)
+        this.setState({
+            show: true,
+            form_mode: roomInfo
+        }, () => {
+            this.setState({
+                show: false,
+                form_mode: roomInfo
+            })
+        })
+    }
+
     render() {
+        const { show, roomInfo, dummy } = this.state
         return (
-            <div>Room List</div>
+            <>
+            {dummy && dummy.map(room => {
+                return (
+                    <Card key={room.id}>
+                        <Card.Body>{room.name}
+                            <Button variant="primary" className="float-right" onClick={() => this.handleShow(room)}>Invite</Button>
+                        </Card.Body>
+                    </Card>
+                )
+            })}
+
+            <ModalRoom show={show} roomInfo={roomInfo}/>
+
+            </>
         )
     }
+
 }
 
 const mapStateToProps = (state) => {
